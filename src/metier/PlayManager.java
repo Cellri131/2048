cff19f2738e16b2122d2e0134827e3ad0db6de78
BARUCHELLO Romain <r_baruchello@hotmail.com>
2026-01-20 00:06:59 +0100

    Ajout de la méthode deplacement dans la classe PlayManager pour gérer les mouvements du joueur

--- src/metier/PlayManager.java
+++ src/metier/PlayManager.java
@@ -19,7 +19,16 @@ public class PlayManager {
         }*/
     }
 
-
+    public int deplacement(){
+        //retourne un int en fonction de la direction du déplacement
+        KeyHandle keyHandle = new KeyHandle();
+        if(KeyHandle.upPressed)return 0;
+        if(KeyHandle.rightPressed)return 1;
+        if(KeyHandle.downPressed)return 2;
+        if(KeyHandle.leftPressed)return 3;
+        
+        return 9;
+    }
     
     public int suite(int val1, int val2){
         int res = val1 + val2;
